5cb3bf20b5f5ed19b8b6c0b11e0ae4c6880ae3c0
Galyna <minimal005@gmail.com>
2023-06-05 19:16:47 +0300

    finished project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.burger').addEventListener('click', function () {
+   this.classList.toggle('active');
+   document.querySelector('.burger__nav').classList.toggle('open');
+});
